ad6a3522cc4c17d6fe43c4b753fa1badf852bd56
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-02-24 10:35:53 -0300

    Captura o nome e exibe a mensagem. Exercicio pronto

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
 public class EntregaGit{
   public static void main (String [] args){
+    String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+    JOptionPane.showMessageDialog(null, String.format("Oi, %s", nome));
     System.out.println ("Entregando tarefas com o Git na linha de comando...");
   }
 }
